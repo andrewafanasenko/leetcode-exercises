426aae4e0647654228cc4e7bb685db16bf119215
Andrii Afanasenko <andrii.afanasenko@gmail.com>
2023-11-05 18:08:08 +0200

    refactoring

--- src/top_interview_questions/13. Roman to Integer.kt
+++ src/top_interview_questions/13. Roman to Integer.kt
@@ -7,35 +7,37 @@ fun main() {
 }
 
 fun romanToInt(s: String): Int {
-    val numeralsMap = hashMapOf(
+    val singleNumeralsMap = hashMapOf(
             "I" to 1,
-            "IV" to 4,
             "V" to 5,
-            "IX" to 9,
             "X" to 10,
-            "XL" to 40,
             "L" to 50,
-            "XC" to 90,
             "C" to 100,
-            "CD" to 400,
             "D" to 500,
-            "CM" to 900,
             "M" to 1000
     )
+    val multipleNumeralsMap = hashMapOf(
+            "IV" to 4,
+            "IX" to 9,
+            "XL" to 40,
+            "XC" to 90,
+            "CD" to 400,
+            "CM" to 900
+    )
     var sum = 0
-    var str = s
-    while (str.isNotEmpty()) {
-        val complexNumber = if (str.length < 2) {
+    var number = s
+    while (number.isNotEmpty()) {
+        val complexNumber = if (number.length < 2) {
             null
         } else {
-            numeralsMap[str.substring(startIndex = 0, endIndex = 2)]
+            multipleNumeralsMap[number.substring(startIndex = 0, endIndex = 2)]
         }
         if (complexNumber != null) {
             sum += complexNumber
-            str = str.substring(2)
+            number = number.substring(2)
         } else {
-            sum += numeralsMap[str[0].toString()]!!
-            str = str.substring(1)
+            sum += singleNumeralsMap[number[0].toString()]!!
+            number = number.substring(1)
         }
     }
     return sum
