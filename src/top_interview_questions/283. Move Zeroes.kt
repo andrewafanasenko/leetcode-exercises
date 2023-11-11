be9b7c72fd304189f8b1fdcb0cd5f890a8529bc0
Andrii Afanasenko <andrii.afanasenko@gmail.com>
2023-11-11 23:24:47 +0200

    283. Move Zeroes

---
+++ src/top_interview_questions/283. Move Zeroes.kt
@@ -0,0 +1,27 @@
+package top_interview_questions
+
+/**
+ * https://leetcode.com/problems/move-zeroes/submissions/1096833845/
+ */
+
+fun main() {
+    println(moveZeroes(intArrayOf(0, 1, 0, 3, 12)))
+    println(moveZeroes(intArrayOf(0)))
+}
+
+fun moveZeroes(nums: IntArray) {
+    val nonZero = mutableListOf<Int>()
+    nums.forEach {
+        if (it != 0) {
+            nonZero.add(it)
+        }
+    }
+    if (nums.size > nonZero.size) {
+        repeat(nums.size - nonZero.size) {
+            nonZero.add(0)
+        }
+    }
+    nonZero.forEachIndexed { index, i ->
+        nums[index] = i
+    }
+}
