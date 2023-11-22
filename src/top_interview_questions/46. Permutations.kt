ab5cf0c27a6a00a91ab169a06aeec7db81ba35c4
Andrii Afanasenko <andrii.afanasenko@gmail.com>
2023-11-22 23:35:46 +0200

    46. Permutations

---
+++ src/top_interview_questions/46. Permutations.kt
@@ -0,0 +1,28 @@
+package top_interview_questions
+
+import java.util.LinkedList
+
+/**
+ * https://leetcode.com/problems/permutations/submissions/1104447035/
+ */
+
+fun main() {
+    println(permute(intArrayOf(1,2,3)))
+    println(permute(intArrayOf(0,1)))
+    println(permute(intArrayOf(1)))
+}
+
+fun permute(nums: IntArray): List<List<Int>> {
+    val result = LinkedList<List<Int>>().apply { add(ArrayList()) }
+    for (num in nums) {
+        for (size in result.size downTo 1) {
+            val firstNum = result.pollFirst()
+            for (i in 0..firstNum.size) {
+                val newCombination = firstNum.toMutableList()
+                newCombination.add(i, num)
+                result.add(newCombination)
+            }
+        }
+    }
+    return result
+}
