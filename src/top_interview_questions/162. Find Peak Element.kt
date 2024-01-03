08943e76ab00fac61ea335f271e1aa771ef248b9
Andrii Afanasenko <andrii.afanasenko@gmail.com>
2024-01-03 22:31:31 +0200

    162. Find Peak Element

---
+++ src/top_interview_questions/162. Find Peak Element.kt
@@ -0,0 +1,28 @@
+package top_interview_questions
+
+/**
+ * https://leetcode.com/problems/find-peak-element/submissions/1135994070/
+ */
+fun main() {
+    println(findPeakElement(intArrayOf(1, 2, 3, 1)))
+    println(findPeakElement(intArrayOf(1, 2, 1, 3, 5, 6, 4)))
+    println(findPeakElement(intArrayOf(1, 2, 1, 3, 5, 6, 4)))
+}
+
+fun findPeakElement(nums: IntArray): Int {
+    var resultIndex = -1
+    var resultValue = Int.MIN_VALUE
+    for (index in nums.indices) {
+        if (resultIndex == -1) {
+            resultIndex = index
+            resultValue = nums[index]
+        } else {
+            val absoluteValue = nums[index]
+            if (absoluteValue > resultValue) {
+                resultValue = absoluteValue
+                resultIndex = index
+            }
+        }
+    }
+    return resultIndex
+}
